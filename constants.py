a1b18ea3924fd097fbdf3d1040d0f5740c1292f3
Sergio Gimenez <sergiogimenezanton@gmail.com>
2019-11-24 21:58:50 +0100

    delete duplcates

--- constants.py
+++
@@ -1,7 +0,0 @@
-
-class Constants:
-    driving = "driving"
-    walking = "walking"
-    bike = "bicycling"
-    public_transport = "transit"
-    list_of_transports = ["driving", "walking", "bicycling", "transit"]
--- initial_form.py
+++
@@ -1,7 +0,0 @@
-class Initialform:
-	startAddress = "La Pau"
-	endAddress = "Col·legi Claret"
-	dayOfSearch = "2019-11-18 20:00:00"
-	dis = True
-	taxi = False
-	ev = False
