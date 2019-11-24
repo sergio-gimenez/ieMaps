3e2ecb9a5d5d3b3b3a826804ab3a4587c4bc42c2
Sergio Gimenez <sergiogimenezanton@gmail.com>
2019-11-24 21:56:20 +0100

    Ordenant una mica el projecte

---
+++ app/constants.py
@@ -0,0 +1,7 @@
+
+class Constants:
+    driving = "driving"
+    walking = "walking"
+    bike = "bicycling"
+    public_transport = "transit"
+    list_of_transports = ["driving", "walking", "bicycling", "transit"]
