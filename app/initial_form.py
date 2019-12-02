d26ae88889c1de68ccd6030df63f75d933038ee0
Sergio Gimenez <sergiogimenezanton@gmail.com>
2019-12-02 15:12:04 +0100

    Afegit hora actual automatica

--- app/initial_form.py
+++ app/initial_form.py
@@ -1,7 +1,10 @@
+from datetime import date
+import datetime
+
 class Initialform:
     startAddress = "La Pau"
     endAddress = "Col·legi Claret"
-    dayOfSearch = "2019-11-27 23:00:00"
+    dayOfSearch = str(datetime.datetime.now()).split(".")[0]    
     dis = True
     taxi = False
     ev = False
