9dfded4b187d1703f747322e62469f95c8d91a18
martiriera <martiriera13@gmail.com>
2019-12-14 12:36:42 +0100

    Start and end coordinates OK. Marker custom on map

--- react/client-web/src/Routes.js
+++ react/client-web/src/Routes.js
@@ -13,7 +13,8 @@ class Routes extends React.Component {
                 <Switch>
                     <Route path="/" exact component={InitalForm} />
                     <Route path="/search" render={(props) => <Search {...props} url={history.location.pathname} />} />
-                    <Route path="/view" render={(props) => <View {...props} lat="41.387705" lon="2.111793"/>} />
+                    {/* <Route path="/view" render={(props) => <View {...props} lat="41.387705" lon="2.111793"/>} /> */}
+                    {/* <Route path="/view/" component={View} /> */}
                 </Switch>
             </Router>
         )
