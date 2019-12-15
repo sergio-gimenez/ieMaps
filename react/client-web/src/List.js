58beae49673832f73b20e230a9bd59c46785cc9e
martiriera <martiriera13@gmail.com>
2019-12-15 22:58:03 +0100

    "New search" button added

--- react/client-web/src/List.js
+++ react/client-web/src/List.js
@@ -4,6 +4,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 // import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import history from './history';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -25,9 +27,9 @@ function getKeys(data) {
 
 export default function SimpleList(props) {
     const classes = useStyles();
-    console.log(getKeys(props.data))
     return (
         <div className={classes.root}>
+            <Button variant="outlined" onClick={() => history.push("/")}>New search</Button>
             <List >
                 {getKeys(props.data)}
             </List>
